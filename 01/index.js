c1ee302035317b66e4782b9ff5169ffddcc580b4
Stefan Haselwanter <stefan.haselwanter@gmail.com>
2021-12-03 23:07:20 +0100

    Add puzzle 01

---
+++ 01/index.js
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("./input.txt", "utf-8");
+const measurements = input.split("\n").map((m) => +m);
+
+let increases = 0;
+
+// Problem 1
+// for (let i = 1; i < measurements.length; i++) {
+//     if (measurements[i] > measurements[i - 1]) {
+//         increases++;
+//     }
+// }
+
+// Problem 2
+for (let i = 0; i < measurements.length - 3; i++) {
+    const windowA = measurements[i] + measurements[i + 1] + measurements[i + 2];
+    const windowB = measurements[i + 1] + measurements[i + 2] + measurements[i + 3];
+
+    if (windowB > windowA) {
+        increases++;
+    }
+}
+
+console.log(increases);
---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code
+
+Checkout the current [event](https://adventofcode.com).
+
+## Puzzles
+
+-   2021:
+    -   [01](https://github.com/Stevemaster92/advent-of-code/01)
